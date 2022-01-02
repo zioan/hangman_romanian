2c510db50f6d5fdafdb929f8b735e3031530e00c
Zaharia Ioan <67908472+zioan@users.noreply.github.com>
2022-01-02 18:02:10 +0100

    autostart

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import os
 from words import word_list
 from art import stages, logo
 
@@ -9,6 +10,10 @@ lives = 6
 
 print(logo)
 # print(chosen_word)
+
+
+def game_restart():
+    os.system("py main.py")
 
 
 display = []
@@ -36,9 +41,11 @@ while not game_over:
             game_over = True
             print("Game over. Ai pierdut!")
             print(f"Cuvantul corect este: {chosen_word}")
+            game_restart()
 
     if "_" not in display:
         game_over = True
+        game_restart()
         print("Ai castigat!")
 
     print(stages[lives])
